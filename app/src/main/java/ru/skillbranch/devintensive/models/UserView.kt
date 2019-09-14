336adbb10e1bacf76a877efd1bae14e299572d11
ildar.serazitdinov@itbrick.ru <XeoARMrV%>
2019-09-14 15:05:23 +0300

    hometask_2 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/UserView.kt
@@ -0,0 +1,21 @@
+package ru.skillbranch.devintensive.models
+
+class UserView (
+    val id : String,
+    val fullName: String,
+    val nickName: String,
+    val avatar: String? = null,
+    val status: String? = "offline",
+    val initials: String?
+) {
+    fun printMe() {
+        println("""
+            id : $id 
+            fullName: $fullName
+            nickName: $nickName
+            avatar: $avatar
+            status: $status
+            initials: $initials
+        """.trimIndent())
+    }
+}
