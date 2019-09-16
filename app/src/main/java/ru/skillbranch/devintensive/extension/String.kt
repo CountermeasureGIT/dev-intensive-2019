5affcb0d554615964db839fedcf84e2178c6dec3
ildar.serazitdinov@itbrick.ru <XeoARMrV%>
2019-09-16 18:48:18 +0300

    Частичная реализация extension String.stripHtml(): String *String.stripHtml

--- app/src/main/java/ru/skillbranch/devintensive/extension/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extension/String.kt
@@ -1,5 +1,9 @@
 package ru.skillbranch.devintensive.extension
 
+import java.util.Collections.replaceAll
+
+
+
 /*Реализуй extension усекающий исходную строку до указанного числа символов (по умолчанию 16) и
 возвращающий усеченную строку с заполнителем "..." (если строка была усечена) если последний символ
 усеченной строки является пробелом - удалить его и добавить заполнитель
@@ -14,4 +18,16 @@ fun String.truncate(size: Int = 16): String {
     if (trimmed.length <= size)
         return trimmed
     return "${this.subSequence(0, size).trimEnd()}..."
+}
+
+/*Реализуй extension позволяющий очистить строку от html тегов и html escape последовательностей
+("& < > ' ""), а так же удалить пустые символы (пробелы) между словами если их больше 1.
+Необходимо вернуть модифицированную строку
+Пример:
+"<p class="title">Образовательное IT-сообщество <p>Skill</p> Branch</p>".stripHtml() //Образовательное IT-сообщество Skill Branch
+"<p>Образовательное       IT-сообщество Skill Branch</p>".stripHtml() //Образовательное IT-сообщество Skill Branch*/
+
+fun String.stripHtml(): String {
+    //TODO maybe later
+    return this.replace("<.*?>".toRegex(), "")
 }
