09acc26b131c6c0a04f8f7379bfa598290791f22
ildar.serazitdinov@itbrick.ru <XeoARMrV%>
2019-09-16 14:04:29 +0300

    Выполнение заданий: parseFullName, *toInitials, *transliteration, **plural

--- app/src/main/java/ru/skillbranch/devintensive/extension/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extension/Date.kt
@@ -2,11 +2,12 @@ package ru.skillbranch.devintensive.extension
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.absoluteValue
 
-const val SECOND = 1000L
-const val MINUTE = 60 * SECOND
-const val HOUR = 60 * MINUTE
-const val DAY = 24 * HOUR
+const val SECOND = 1000L        //1_000
+const val MINUTE = 60 * SECOND  //60_000
+const val HOUR = 60 * MINUTE    //3_600_000
+const val DAY = 24 * HOUR       //86_400_000
 
 fun Date.format(pattern : String = "HH:mm:ss dd.MM.yy") : String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
@@ -26,14 +27,84 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND) : Date {
     return this
 }
 
-enum class TimeUnits {
-    SECOND,
-    MINUTE,
-    HOUR,
-    DAY
+/*Реализуй метод plural для всех перечислений TimeUnits следующего вида TimeUnits.SECOND.plural(value:Int) возвращающую значение в виде строки с праильно склоненной единицой измерения
+Пример:
+TimeUnits.SECOND.plural(1) //1 секунду
+TimeUnits.MINUTE.plural(4) //4 минуты
+TimeUnits.HOUR.plural(19) //19 часов
+TimeUnits.DAY.plural(222) //222 дня*/
+
+fun getPlural(value: Int): Int {
+    return if (value % 10 == 1 && value % 100 != 11) //1, 21, 31, 41...
+        0
+    else if (value % 10 in 2..4 && (value % 100 < 10 || value % 100 >= 20)) //2, 3, 4, 12, 13, 14, 22, 23, 24...
+        1
+    else
+        2 //5, 6, 7, 8, 9, 10, 15, 16, 17, 18, 19, 20...
 }
 
+enum class TimeUnits {
+    SECOND {
+        override fun plural(value: Int): String {
+            return when(getPlural(value)) {
+                0 -> "$value секунду"
+                1 -> "$value секунды"
+                2 -> "$value секунд"
+                else -> "$value сек."
+            }
+        }
+    },
+    MINUTE {
+        override fun plural(value: Int): String {
+            return when(getPlural(value)) {
+                0 -> "$value минуту"
+                1 -> "$value минуты"
+                2 -> "$value минут"
+                else -> "$value мин."
+            }
+        }
+    },
+    HOUR {
+        override fun plural(value: Int): String {
+            return when(getPlural(value)) {
+                0 -> "$value час"
+                1 -> "$value часа"
+                2 -> "$value часов"
+                else -> "$value ч."
+            }
+        }
+    },
+    DAY {
+        override fun plural(value: Int): String {
+            return when(getPlural(value)) {
+                0 -> "$value день"
+                1 -> "$value дня"
+                2 -> "$value дней"
+                else -> "$value д."
+            }
+        }
+    };
 
-fun Date.humanizeDiff(date : Date = Date()): String {
-    return ""
+    abstract fun plural(value: Int): String
+}
+
+/*Реализуй extension Date.humanizeDiff(date) (значение по умолчанию текущий момент времени) для
+форматирования вывода разницы между датами в человекообразном формате, с учетом склонения числительных.
+Временные интервалы преобразований к человекообразному формату доступны в ресурсах к заданию
+Пример:
+Date().add(-2, TimeUnits.HOUR).humanizeDiff() //2 часа назад
+Date().add(-5, TimeUnits.DAY).humanizeDiff() //5 дней назад
+Date().add(2, TimeUnits.MINUTE).humanizeDiff() //через 2 минуты
+Date().add(7, TimeUnits.DAY).humanizeDiff() //через 7 дней
+Date().add(-400, TimeUnits.DAY).humanizeDiff() //более года назад
+Date().add(400, TimeUnits.DAY).humanizeDiff() //более чем через год*/
+fun Date.humanizeDiff(date: Date = Date()): String {
+    val diff = this.time - date.time
+    var str: String
+    str = if (diff >= 0) {
+        "через $diff миллисекунд"
+    } else {
+        "${diff.absoluteValue} миллисекунд назад"
+    }
+    return str
 }
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,20 +1,93 @@
 package ru.skillbranch.devintensive.utils
 
+import java.lang.StringBuilder
+import java.util.*
+
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        val parts : List<String>? = fullName?.split(" ")
+        if (fullName == null || fullName.trim().isEmpty())
+            return null to null
 
-        val firstName = parts?.getOrNull(0) ?: ""
-        val lastName = parts?.getOrNull(1) ?: ""
+        val parts: List<String>? = fullName.trim().split(" ")
+
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
 
         return firstName to lastName
     }
 
-    fun transliteration(payload: String, divider: String = " "): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        return "${firstName?.getOrNull(0)?.toUpperCase()
+            ?: ""}${lastName?.getOrNull(0)?.toUpperCase() ?: ""}"
     }
 
-    fun toInitials(firstName: String?, lastName: String?): String? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun cyrillicToLat(ch : Char) : String {
+        val latChar = when (ch) {
+            'а' -> "a"
+            'б' -> "b"
+            'в' -> "v"
+            'г' -> "g"
+            'д' -> "d"
+            'е' -> "e"
+            'ё' -> "e"
+            'ж' -> "zh"
+            'з' -> "z"
+            'и' -> "i"
+            'й' -> "i"
+            'к' -> "k"
+            'л' -> "l"
+            'м' -> "m"
+            'н' -> "n"
+            'о' -> "o"
+            'п' -> "p"
+            'р' -> "r"
+            'с' -> "s"
+            'т' -> "t"
+            'у' -> "u"
+            'ф' -> "f"
+            'х' -> "h"
+            'ц' -> "c"
+            'ч' -> "ch"
+            'ш' -> "sh"
+            'щ' -> "sh'"
+            'ъ' -> ""
+            'ы' -> "i"
+            'ь' -> ""
+            'э' -> "e"
+            'ю' -> "yu"
+            'я' -> "ya"
+            else -> ch.toString()
+        }
+        return latChar
+    }
+
+/*Реализуй метод Utils.transliteration(payload divider) принимающий в качестве аргумента строку (divider по умолчанию " ") и возвращающий преобразованную строку из латинских символов, словарь символов соответствия алфовитов доступен в ресурсах к заданию
+Пример:
+Utils.transliteration("Женя Стереотипов") //Zhenya Stereotipov
+Utils.transliteration("Amazing Петр","_") //Amazing_Petr*/
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        val words = payload.split(" ").toList()
+        val sbTemp = StringBuilder()
+        val sbResult = StringBuilder()
+
+        words.getOrNull(0)?.toLowerCase(Locale.ROOT)?.forEach { ch ->
+            sbTemp.append(cyrillicToLat(ch))
+        }
+        if (sbTemp.isNotEmpty()) {
+            sbTemp[0] = sbTemp[0].toUpperCase()
+            sbResult.append(sbTemp)
+            sbTemp.clear()
+        }
+        words.getOrNull(1)?.toLowerCase(Locale.ROOT)?.forEach { ch ->
+            sbTemp.append(cyrillicToLat(ch))
+        }
+        if (sbTemp.isNotEmpty()) {
+            if (sbResult.isNotEmpty())
+                sbResult.append(divider)
+            sbTemp[0] = sbTemp[0].toUpperCase()
+            sbResult.append(sbTemp)
+        }
+        return sbResult.toString()
     }
 }
