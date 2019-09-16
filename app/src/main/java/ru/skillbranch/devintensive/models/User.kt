cac1a4106527a4cb638b37899714ff9686e0fbee
ildar.serazitdinov@itbrick.ru <XeoARMrV%>
2019-09-16 16:41:17 +0300

    Рализация паттерна Builder для класса User **Паттерн Builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -21,19 +21,64 @@ data class User(
     constructor(id: String) : this(id, "John", "Doe")
 
     init {
-        println("It's Alive!!! \n" +
-                "${if(lastName==="Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName!!!"}\n"
+        println(
+            "It's Alive!!! \n" +
+                    "${if (lastName === "Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName!!!"}\n"
         )
     }
 
     companion object Factory {
         private var lastId: Int = -1
 
-        fun makeUser(fullName: String?) : User{
+        fun makeUser(fullName: String?): User {
             lastId++
 
             val (firstName, lastName) = Utils.parseFullName(fullName)
             return User(id = "$lastId", firstName = firstName, lastName = lastName)
         }
     }
+
+/*Реализуй паттерн Builder для класса User.
+User.Builder().id(s)
+.firstName(s)
+.lastName(s)
+.avatar(s)
+.rating(n)
+.respect(n)
+.lastVisit(d)
+.isOnline(b)
+.build() должен вернуть объект User*/
+
+    class Builder {
+        var id: String = "0"
+        var firstName: String? = null
+        var lastName: String? = null
+        var avatar: String? = null
+        var rating: Int = 0
+        var respect: Int = 0
+        var lastVisit: Date? = null
+        var isOnline: Boolean = false
+
+        fun id(id: String) = apply { this.id = id }
+        fun firstName(firstName: String) = apply { this.firstName = firstName }
+        fun lastName(lastName: String) = apply { this.lastName = lastName }
+        fun avatar(avatar: String) = apply { this.avatar = avatar }
+        fun rating(rating: Int) = apply { this.rating = rating }
+        fun respect(respect: Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit: Date?) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+        fun build() = User(this)
+
+    }
+
+    private constructor(builder: Builder) : this(
+        builder.id,
+        builder.firstName,
+        builder.lastName,
+        builder.avatar,
+        builder.rating,
+        builder.respect,
+        builder.lastVisit,
+        builder.isOnline
+    )
 }
